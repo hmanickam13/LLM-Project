4f3e4072c027b4b582e4f98acd7342a2f2eb7f73
hmanickam13 <hmanickam13@gmail.com>
2023-05-20 19:50:20 -0500

    modified comments for cat line

--- myproject/scripts/setup.sh
+++ myproject/scripts/setup.sh
@@ -7,5 +7,5 @@ source ghcproject/bin/activate
 # upgrade pip
 pip install --upgrade pip
 
-# install project dependencies
+# install project dependencies (refers to requirements.txt)
 cat requirements.txt | sed -e '/^\s*#.*$/d' -e '/^\s*$/d' | xargs -n 1 python -m pip install
